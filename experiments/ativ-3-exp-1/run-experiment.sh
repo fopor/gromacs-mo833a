371ad4775f7ead9067382b70c41c2ca7c236361e
Rodrigo Ceccato de Freitas <rodrigoceccatodefreitas@gmail.com>
2020-04-03 10:49:18 -0300

    Fix script paths

--- experiments/ativ-3-exp-1/run-experiment.sh
+++ experiments/ativ-3-exp-1/run-experiment.sh
@@ -8,16 +8,30 @@ cd build/debug/bin # go to build dir
 ls
 echo "" > output.log
 
-for ((a=0; a <= 10000; a++))
-do
-    ./gmx mdrun -v -deffnm em >> output.log
-done
-
 cd $scriptDir
 cd ../../       # go to repo root
 cd build/release/bin # go to build dir
+echo "" > output.log
+cd $scriptDir
+sensors > temp_output.log
+
 for ((a=0; a <= 10000; a++))
 do
-./gmx mdrun -v -deffnm em
+    echo "DEBUG RUN $a"
+    cd $scriptDir
+    date >> temp_output.log
+    sensors >> temp_output.log
+    cd ../../       # go to repo root
+    cd build/debug/bin # go to build dir
+    date >> output.log
+    ./gmx mdrun -v -deffnm em >> output.log
+    cd $scriptDir
+    cd ../../       # go to repo root
+    cd build/release/bin # go to build dir
+    date >> output.log
+    ./gmx mdrun -v -deffnm em >> output.log
+    cd $scriptDir
 done
+
+
 
