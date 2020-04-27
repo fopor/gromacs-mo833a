ead93febf5b3a2f4d4de190ebcc14f6b90b2f465
Rodrigo Freitas <rodrigoceccatodefreitas@gmail.com>
2020-04-27 01:13:33 -0300

    remove duplicates from file

--- experiments/ativ-4-exp-1/run-experiments.sh
+++ experiments/ativ-4-exp-1/run-experiments.sh
@@ -1,6 +1,4 @@
 PATH_TO_BUILD='../../build/bin'
 
 perf record -a -g -o perf_sthread.data $PATH_TO_BUILD/gmx mdrun -v -deffnm em
-perf record -a -g -o perf_mthread.data $PATH_TO_BUILD/gmx mdrun -v -deffnm em
-valgrind --tool=callgrind $PATH_TO_BUILD/gmx mdrun -v -deffnm em
 valgrind --tool=callgrind $PATH_TO_BUILD/gmx mdrun -v -deffnm em
