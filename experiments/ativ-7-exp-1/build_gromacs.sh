51088b909e672d1c0a2a9a520ca452084488afbd
Rodrigo Ceccato de Freitas <rodrigoceccatodefreitas@gmail.com>
2020-05-17 16:15:36 -0300

    Update build script

--- experiments/ativ-7-exp-1/build_gromacs.sh
+++ experiments/ativ-7-exp-1/build_gromacs.sh
@@ -1,9 +1,29 @@
-BUILD_DIR='../../build'
+#!/bin/bash
+# Install requirements
+echo "Installing dependencie..." >> /home/ubuntu/progress.txt
+sudo apt update
+sudo apt install -y wget make gcc libgfortran3 sysstat libibnetdisc-dev openmpi-bin libopenmpi-dev libhdf5-openmpi-dev gfortran build-essential git cmake 
 
-mkdir -p $BUILD_DIR
-cd $BUILD_DIR
-cmake .. -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON
-make -j 10
+# Set SSH key
+echo "Setting SSH key..." >> /home/ubuntu/progress.txt
+ssh-keygen -t rsa -q -f "/home/ubuntu/.ssh/id_rsa" -N ""
+cat /home/ubuntu/.ssh/id_rsa.pub >> /home/ubuntu/.ssh/authorized_keys
 
-# add curent folder to PATH
-PATH=`pwd`:$PATH
+cd ../../
+mkdir build
+cd build
+echo "Building Gromacs..." >> /home/ubuntu/progress.txt
+cmake .. -DGMX_BUILD_OWN_FFTW=ON -DREGRESSIONTEST_DOWNLOAD=ON -DGMX_MPI=on
+make
+sudo make install
+source /usr/local/gromacs/bin/GMXRC
+sudo -l echo "source /usr/local/gromacs/bin/GMXRC" >> ~/.bashrc
+cd ~
+echo "Removing source files" >> /home/ubuntu/progress.txt
+rm ~/gromacs-2020.2 -rf # remove source files
+
+# Downloading the simulation input
+echo "Downloading the simulation input!" >> /home/ubuntu/progress.txt
+wget https://github.com/fopor/gromacs-mo833a/raw/ativi-5-exp-1/experiments/ativ-5-exp-1/em.tpr -P /home/ubuntu
+
+echo "Done!" >> /home/ubuntu/progress.txt
